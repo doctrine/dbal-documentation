e8a8cd1d569e03514dc015030b322792fd2c1beb
Benjamin Eberlei <kontakt@beberlei.de>
2012-02-05 21:11:05 +0100

    Update theme

--- en/conf.py
+++ en/conf.py
@@ -124,6 +124,7 @@ html_theme_path = ['_theme']
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_static']
 
+
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 #html_last_updated_fmt = '%b %d, %Y'
